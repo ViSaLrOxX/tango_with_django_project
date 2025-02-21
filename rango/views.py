04f1152d89c6d2357c036b48e0bc3da76050d6b4
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-02-21 14:47:39 +0000

    Update views.py after chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from rango.models import Category, Page
+from rango.models import Page, Category
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-                    'categories': category_list,
-                    'pages': page_list}
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': page_list
+    }
 
     return render(request, 'rango/index.html', context=context_dict)
 
+def about(request):
+    return render(request, 'rango/about.html')
 
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict = {'category': category, 'pages': pages}
+    except Category.DoesNotExist:
+        context_dict = {'category': None, 'pages': None}
+
+    return render(request, 'rango/category.html', context=context_dict)
