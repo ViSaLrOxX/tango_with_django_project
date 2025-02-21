238e608bb3592179c6aa8f39387a25cc178f036c
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-02-21 14:11:08 +0000

    0002_update_category_fields.py after chapter 5

---
+++ rango/migrations/0002_update_category_fields.py
@@ -0,0 +1,29 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='category',
+            options={'verbose_name_plural': 'Categories'},
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='views',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='likes',
+            field=models.IntegerField(default=0),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(unique=True, blank=True),
+        ),
+    ]
