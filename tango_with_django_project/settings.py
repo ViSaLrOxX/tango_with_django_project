375c56c0ccc37e831aea0c81a2069b2b25240394
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-02-21 15:57:35 +0000

    Update settings.py after chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -82,6 +82,11 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -91,6 +96,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
+
+LOGIN_URL = 'rango:login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
