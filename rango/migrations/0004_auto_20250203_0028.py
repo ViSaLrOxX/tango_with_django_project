19fd19a949b83e672c0937f3ecdb02de463a28fb
ViSaLrOxX <123654528+ViSaLrOxX@users.noreply.github.com>
2025-02-21 14:53:26 +0000

    0004_auto_20250203_0028.py

---
+++ rango/migrations/0004_auto_20250203_0028.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0003_category_slug'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(unique=True),
+        ),
+    ]
